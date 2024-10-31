856394608148c6965d1567f204cc9b535c6a0a2f
Gael Chavez <gael.chavez.h@gmail.com>
2024-10-30 22:47:33 -0400

    Hello world from mac

--- src/CS131/Exam2.java
+++ src/CS131/Exam2.java
@@ -3,5 +3,7 @@ package CS131;
 public class Exam2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Hello World - From mac");
     }
 }
