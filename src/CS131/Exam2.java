6603e2a5624e3a03710e171f2e0b0491a4ecb541
GaelHCH <gael.chavez.h@gmail.com>
2024-10-30 22:49:04 -0400

    First commit change

--- src/CS131/Exam2.java
+++ src/CS131/Exam2.java
@@ -2,7 +2,7 @@ package CS131;
 
 public class Exam2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World - From windows");
 
         System.out.println("Hello World - From mac");
     }
